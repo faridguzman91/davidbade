0fa45deab95df767acc388939092b3179ccb46b5
Jimmy Guzman <jimmy@creapolis.com>
2021-02-13 15:31:19 +0100

    david bade index1+index2 html, refactoring code

--- js/timelineswiper.js
+++ js/timelineswiper.js
@@ -1,21 +1,18 @@
-/* timeline swiper js */
-
-var timelineSwiper = new Swiper(".timeline .swiper-container", {
-    direction: "vertical",
+var timelineSwiper = new Swiper('.timeline .swiper-container', {
+    direction: 'vertical',
     loop: false,
     speed: 1600,
-    pagination: ".swiper-pagination",
+    pagination: '.swiper-pagination',
     paginationBulletRender: function(swiper, index, className) {
-        var year = document
-            .querySelectorAll(".swiper-slide")[index].getAttribute("data-year");
-        return '<span class="' + className + '">' + year + "</span>";
+        var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+        return '<span class="' + className + '">' + year + '</span>';
     },
     paginationClickable: true,
-    nextButton: ".swiper-button-next",
-    prevButton: ".swiper-button-prev",
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
     breakpoints: {
         768: {
-            direction: "horizontal"
+            direction: 'horizontal',
         }
     }
 });
